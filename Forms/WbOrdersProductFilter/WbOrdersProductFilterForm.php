70ade9fc0c8d3ab46bad9a69123541da8cbb7bdc
kingstar <admin@baks.dev>
2025-01-16 16:37:08 +0300

    Baks Development

--- Forms/WbOrdersProductFilter/WbOrdersProductFilterForm.php
+++ Forms/WbOrdersProductFilter/WbOrdersProductFilterForm.php
@@ -90,7 +90,7 @@ final class WbOrdersProductFilterForm extends AbstractType
                         return $category?->getValue();
                     },
                     'choice_label' => function(CategoryProductUid $category) {
-                        return ($category->getAttr() ? str_repeat('- &nbsp; &nbsp;', $category->getAttr() - 1) : '').$category->getOptions();
+                        return (is_int($category->getAttr()) ? str_repeat(' - ', $category->getAttr() - 1) : '').$category->getOptions();
                     },
                     'label' => false,
                     'required' => false,
